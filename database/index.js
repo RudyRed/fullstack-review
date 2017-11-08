80a9e2a5f435669419d084fd8178fead0b6928f0
RudyRed <willgross94@gmail.com>
2017-11-08 14:38:08 -0800

    Set up schema: Untested

--- database/index.js
+++ database/index.js
@@ -2,15 +2,51 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number, // id
+  name: String, //reponame
+  isFork: Boolean, // fork
+  repoUrl: String, // html_url
+  stargazers: Number, // stargazers_count
+  forks: Number, // forks
+  ownerMeta: { // Within the owner obj in api data
+    username: String,
+    ownerUrl: String,
+    avatarUrl: String
+  }
 });
+
+repoSchema.methods.speak = function () {
+  var greeting = this.name && this.owner.name
+    ? `${this.owner.name}'s repo ${this.name} was successully added!`
+    : "owner or repo name missing";
+  console.log(greeting);
+}
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (apiArr) => {
+  for (var item of apiArr) {
+    var newRepo = new Repo({
+      id: item.id, // id
+      name: item.name, //reponame
+      isFork: item.fork, // fork
+      repoUrl: item.html_url, // html_url
+      stargazers: item.stargazers_count, // stargazers_count
+      forks: item.forks, // forks
+      ownerMeta: { // Within the owner obj in api data
+        username: item.owner.login,
+        ownerUrl: item.owner.html_url,
+        avatarUrl: item.owner.avatar_url
+      }
+    })
+    newRepo.save((err, newRepo) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      newRepo.speak();
+    })
+  }
 }
 
 module.exports.save = save;
