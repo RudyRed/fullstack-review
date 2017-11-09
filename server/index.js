1792f5716feaa45fefc5a4545ff0bd3cecaff6c6
RudyRed <willgross94@gmail.com>
2017-11-08 16:59:41 -0800

    set up requires

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 let app = express();
+const Promise = require("bluebird");
+const github = require('../helpers/github.js');
+const db = require('../database/index.js');
+const bodyParser = require('body-parser')
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.json())
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  console.log(req.body)
 });
 
 app.get('/repos', function (req, res) {
@@ -20,4 +26,3 @@ let port = 1128;
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
-
