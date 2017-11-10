fd20c45d47f92688c2e34ca5875f6eeaa1c8d124
RudyRed <willgross94@gmail.com>
2017-11-09 16:36:10 -0800

    added hyperlinks

--- client/src/components/RepoListItem.jsx
+++ client/src/components/RepoListItem.jsx
@@ -8,12 +8,20 @@ const RepoListItem = (props) => {
   return (<div>
     <ul id="menu">
       <li>Ranking: {props.ranking.num}  </li>
-      <li>Repo: {props.repos.name}</li>
-      <li>Repo Owner: {props.repos.ownerMeta.name}  </li>
-      <li><img src={props.repos.ownerMeta.avatarUrl} alt="alternative text for your image" height="42" width="42"/></li>
+      <li><a href={props.repos.repoUrl} >Repo: {props.repos.name} </a></li>
+      <li><a href={props.repos.ownerMeta.ownerUrl} >Repo Owner: {props.repos.ownerMeta.name}  </a></li>
+      <li><a href={props.repos.ownerMeta.ownerUrl} ><img src={props.repos.ownerMeta.avatarUrl} alt="alternative text for your image" height="42" width="42"/></a></li>
       <li>  Stargazers: {props.repos.stargazers}</li>
     </ul>
   </div>)
 
 }
 export default RepoListItem;
+
+
+{/* <ul>
+    <li><a href="#">Link 1</a></li>
+    <li><a href="#">Link 2</a></li>
+</ul>
+
+href={props.repos.repoUrl} */}
